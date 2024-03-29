47feb79e1e64289dff9b5d753922499b458fb099
Ankit Saini <nnkitsaini@gmail.com>
2024-03-29 13:38:24 +0530

    icons

---
+++ lib/app_list.dart
@@ -0,0 +1,123 @@
+import 'package:device_apps/device_apps.dart';
+import 'package:cache_manager/cache_manager.dart';
+// import 'package:json_serializable/builder.dart';
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+// import 'package:json_serializable/json_serializable.dart';
+
+class CacheData {
+  final List<GridApp> grid;
+  List<App> apps;
+
+  CacheData(this.grid, this.apps);
+
+  factory CacheData.fromJson(Map<String, dynamic> json) => CacheData(
+      (json['grid'] as List).map((x) => gridAppFromJson(x)).toList(),
+      (json['apps'] as List).map((x) => App.fromJson(x)).toList());
+
+  static gridAppToJson(GridApp gridApp) {
+    return gridApp.toJson();
+  }
+
+  static GridApp gridAppFromJson(Map<String, dynamic> json) {
+    return GridApp.fromJson(json);
+  }
+
+  Map<String, dynamic> toJson() {
+    return {
+      'grid': grid.map((x) => gridAppToJson(x)).toList(),
+      'apps': apps.map((x) => x.toJson()).toList()
+    };
+  }
+}
+
+class GridApp {
+  final App app;
+  int? iconCodePoint;
+
+  GridApp(this.app, this.iconCodePoint);
+
+  factory GridApp.fromJson(Map<String, dynamic> json) {
+    return GridApp(App.fromJson(json['app']), json['iconCodePoint']);
+  }
+
+  Map<String, dynamic> toJson() {
+    return {'app': app.toJson(), 'iconCodePoint': iconCodePoint};
+  }
+}
+
+class App {
+  final String appName, packageName;
+
+  App(this.appName, this.packageName);
+
+  factory App.fromJson(Map<String, dynamic> json) =>
+      App(json['appName'], json['packageName']);
+
+  factory App.fromApplication(Application application) =>
+      App(application.appName, application.packageName);
+
+  Map<String, dynamic> toJson() {
+    return {'appName': appName, 'packageName': packageName};
+  }
+}
+
+class AppListCacher extends ChangeNotifier {
+  String cacheKey = 'app_list_cache_v3';
+  CacheData? data;
+
+  Future readCache() async {
+    dynamic value = await ReadCache.getString(key: cacheKey);
+    if (value != null) {
+      print("Reading from cache");
+      data = CacheData.fromJson(jsonDecode(value));
+      notifyListeners();
+    } else {
+      print("Cache read miss");
+    }
+  }
+
+  Future<List<App>> getAppList() async {
+    if (data == null) {
+      await readCache();
+    }
+    // Do ~blocking get if there was nothing in cache
+    if (data == null) {
+      await updateCache();
+    } else {
+      updateCache();
+    }
+
+    return data!.apps;
+  }
+
+  Future<List<Application>> _readApps() async {
+    List<Application> apps = (await DeviceApps.getInstalledApplications(
+      includeSystemApps: true,
+      onlyAppsWithLaunchIntent: true,
+    ));
+    return apps;
+  }
+
+  flushChanges() {
+    assert(data != null);
+    WriteCache.setString(key: cacheKey, value: jsonEncode(data!.toJson()));
+    print("cache updated");
+    notifyListeners();
+  }
+
+  Future updateCache() async {
+    print("updating cache");
+    List<App> apps = (await _readApps())
+        .where((x) => x.enabled)
+        .map((x) => App.fromApplication(x))
+        .toList();
+    if (data == null) {
+      data = CacheData([], apps);
+    } else {
+      data!.apps = apps;
+    }
+    flushChanges();
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,13 @@
+import 'package:android_intent_plus/android_intent.dart';
+import 'package:android_intent_plus/flag.dart';
 import 'package:flutter/material.dart';
+import 'package:device_apps/device_apps.dart';
+import 'package:provider/provider.dart';
+import 'package:flutter_iconpicker/flutter_iconpicker.dart';
+
+import 'app_list.dart';
+
+// AppListCacher appList = AppListCacher();
 
 void main() {
   runApp(const MyApp());
@@ -11,7 +20,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Nkit Launcher',
       theme: ThemeData(
         // This is the theme of your application.
         //
@@ -28,10 +37,13 @@ class MyApp extends StatelessWidget {
         //
         // This works for code too, not just values: Most code changes can be
         // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepOrange),
+        brightness: Brightness.light,
+        // brightness:
+        //     SchedulerBinding.instance.platformDispatcher.platformBrightness,
+        // colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepOrange),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'Nkit Launcher Home Page'),
     );
   }
 }
@@ -55,71 +67,452 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  String? filterSearch;
+  TextEditingController textController = TextEditingController();
+  UnfocusDisposition disposition = UnfocusDisposition.scope;
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+  @override
+  Widget build(BuildContext context) {
+    unfocusTextField() {
+      textController.clear();
+      primaryFocus!.unfocus(disposition: disposition);
+      setState(() {
+        filterSearch = null;
+      });
+    }
+
+    List<Widget> children = [];
+    if (filterSearch == null) {
+      children.add(const Expanded(child: IconAppGridWidget()));
+    } else {
+      children.add(Expanded(
+        child: AppListWidget(
+          filterKey: filterSearch,
+          onAppOpen: () {
+            unfocusTextField();
+          },
+        ),
+      ));
+      children.add(IconButton(
+        icon: const Icon(Icons.close),
+        onPressed: () {
+          unfocusTextField();
+        },
+      ));
+    }
+
+    children.add(Row(
+      children: [
+        Expanded(
+          child: Consumer<AppListCacher>(builder: (context, appList, child) {
+            int appCount = 0;
+            if (appList.data != null) {
+              appCount = appList.data!.apps.length;
+            }
+
+            return TextField(
+              controller: textController,
+              decoration: InputDecoration(
+                border: const UnderlineInputBorder(),
+                contentPadding: const EdgeInsets.all(8.0),
+                hintText: 'Search $appCount apps',
+              ),
+              onChanged: (value) => {
+                setState(() {
+                  filterSearch = value;
+                })
+              },
+              onTap: () {
+                setState(() {
+                  filterSearch = '';
+                });
+              },
+              autocorrect: false,
+              autofocus: false,
+              // focusNode: messageFocus,
+            );
+          }),
+        ),
+        Consumer<AppListCacher>(builder: (context, appList, child) {
+          return IconButton(
+            icon: const Icon(Icons.more_vert),
+            tooltip: "Options",
+            onPressed: () async {
+              GlobalAction? action = await askGlobalAction(context);
+              switch (action) {
+                case GlobalAction.refreshApps:
+                  await appList.updateCache();
+                case null:
+              }
+            },
+          );
+        })
+      ],
+    ));
+
+    return Scaffold(
+        body: ChangeNotifierProvider(
+            create: (context) {
+              AppListCacher cacher = AppListCacher();
+              cacher.getAppList(); // TODO: handle error
+              return cacher;
+            },
+            child: Column(
+              children: children,
+            )));
+  }
+}
+
+class AppListWidget extends StatefulWidget {
+  const AppListWidget(
+      {super.key, required this.filterKey, required this.onAppOpen});
+
+  // This widget is the home page of your application. It is stateful, meaning
+  // that it has a State object (defined below) that contains fields that affect
+  // how it looks.
+
+  // This class is the configuration for the state. It holds the values (in this
+  // case the title) provided by the parent (in this case the App widget) and
+  // used by the build method of the State. Fields in a Widget subclass are
+  // always marked "final".
+
+  final String? filterKey;
+  final Function() onAppOpen;
+
+  @override
+  State<AppListWidget> createState() => AppListState();
+}
+
+class IconAppGridWidget extends StatelessWidget {
+  const IconAppGridWidget({super.key}) : super();
+
+  @override
+  Widget build(BuildContext context) {
+    return Consumer<AppListCacher>(builder: (context, appList, child) {
+      if (appList.data == null) {
+        return const Row(children: []);
+      }
+
+      longPressActionHandler(int index) async {
+        GridIconAction? action = await askGridIconAction(
+            context, appList.data!.grid[index].app.appName);
+        switch (action) {
+          case GridIconAction.selectIcon:
+            IconData? icon = await showIconPicker(context, customIconPack: {
+              'photo_camera': Icons.photo_camera,
+              'camera': Icons.camera,
+              'call': Icons.call,
+              'music': Icons.headset,
+              'credit_card': Icons.credit_card,
+              'wb_twilight': Icons.wb_twilight,
+              'podcasts': Icons.podcasts,
+            });
+            if (icon != null) {
+              appList.data!.grid[index].iconCodePoint = icon.codePoint;
+            }
+            appList.flushChanges();
+          case GridIconAction.remove:
+            appList.data!.grid.removeAt(index);
+            appList.flushChanges();
+          case GridIconAction.moveToLeft:
+            if (index != 0) {
+              GridApp app = appList.data!.grid.removeAt(index);
+              appList.data!.grid.insert(index - 1, app);
+              appList.flushChanges();
+            }
+          case GridIconAction.moveToRight:
+            if (index != appList.data!.grid.length - 1) {
+              GridApp app = appList.data!.grid.removeAt(index);
+              appList.data!.grid.insert(index + 1, app);
+              appList.flushChanges();
+            }
+          case null:
+        }
+      }
+
+      List<GridApp> apps = appList.data!.grid;
+
+      List<Widget> children = [];
+      const iconSize = 48.0;
+      for (final (index, app) in apps.indexed) {
+        IconButton button;
+        Widget icon;
+        if (app.iconCodePoint == null) {
+          icon = const Icon(Icons.bolt, size: iconSize);
+        } else {
+          icon = Icon(IconData(app.iconCodePoint!, fontFamily: 'MaterialIcons'),
+              size: iconSize);
+        }
+        button = IconButton(
+          icon: icon,
+          onPressed: () {
+            DeviceApps.openApp(app.app.packageName);
+          },
+        );
+
+        children.add(GestureDetector(
+            child: button,
+            onLongPress: () {
+              longPressActionHandler(index);
+            }));
+      }
+      return GridView.count(
+        crossAxisCount: 2,
+        children: children,
+      );
     });
+  }
+}
+// class AppListState extends State<AppListWidget> {
+//   Widget projectWidget() {
+//     // messageFocus.requestFocus();
+//     return FutureBuilder(
+//       initialData: const <App>[],
+//       builder: (context, projectSnap) {
+//         // messageFocus.requestFocus();
+//         if (projectSnap.connectionState == ConnectionState.none &&
+//             projectSnap.hasData == false) {
+//           return const Text('connection none');
+//         }
+
+//         if (projectSnap.data == null) {
+//           return const Text('data null');
+//         }
+
+//         List<App> apps = projectSnap.data as List<App>;
+
+//         bool isFiltered = widget.filterKey != '';
+//         apps.sort(
+//           (a, b) => a.appName.toLowerCase().compareTo(b.appName.toLowerCase()),
+//         );
+//         if (isFiltered) {
+//           apps = apps.reversed.toList();
+//         }
+//         apps.removeWhere((app) => !app.appName
+//             .toLowerCase()
+//             .contains(widget.filterKey.toLowerCase()));
+//         print('App length: ${apps.length}');
+//         return ListView.builder(
+//           reverse: isFiltered,
+//           itemCount: apps.length,
+//           itemBuilder: (context, index) {
+//             App app = apps[index];
+//             return GestureDetector(
+//               child: ListTile(
+//                 title:
+//                     Text(app.appName, style: const TextStyle(fontSize: 18.0)),
+//               ),
+//               onTap: () => {DeviceApps.openApp(app.packageName)},
+//               onLongPress: () {
+//                 AndroidIntent intent = AndroidIntent(
+//                   action: 'action_application_details_settings',
+//                   data: 'package:${app.packageName}',
+//                   flags: <int>[Flag.FLAG_ACTIVITY_NEW_TASK],
+//                 );
+//                 intent.launch();
+//               },
+//             );
+//           },
+//         );
+//       },
+//       future: appList.getAppList(),
+//     );
+//   }
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       body: projectWidget(),
+//     );
+//   }
+// }
+
+enum GridIconAction { selectIcon, moveToLeft, moveToRight, remove }
+
+Future<GridIconAction?> askGridIconAction(
+    BuildContext context, String title) async {
+  return await showDialog<GridIconAction>(
+      context: context,
+      builder: (BuildContext context) {
+        List<Widget> children = [
+          SimpleDialogOption(
+              onPressed: () =>
+                  Navigator.pop(context, GridIconAction.selectIcon),
+              child: const Text('Select Icon')),
+          SimpleDialogOption(
+              onPressed: () => Navigator.pop(context, GridIconAction.remove),
+              child: const Text('Remove')),
+          SimpleDialogOption(
+              onPressed: () =>
+                  Navigator.pop(context, GridIconAction.moveToLeft),
+              child: const Text('Move to Left')),
+          SimpleDialogOption(
+              onPressed: () =>
+                  Navigator.pop(context, GridIconAction.moveToRight),
+              child: const Text('Move to Right')),
+        ];
+        return SimpleDialog(
+          title: Text(title),
+          children: children,
+        );
+      });
+}
+
+enum GlobalAction { refreshApps }
+
+Future<GlobalAction?> askGlobalAction(BuildContext context) async {
+  return await showDialog<GlobalAction>(
+      context: context,
+      builder: (BuildContext context) {
+        List<Widget> children = [
+          SimpleDialogOption(
+              onPressed: () => Navigator.pop(context, GlobalAction.refreshApps),
+              child: const Text('Refresh Apps')),
+        ];
+        return SimpleDialog(children: children);
+      });
+}
+
+enum AppAction { addToGrid, openAppSettings }
+
+Future<AppAction?> askAppActions(BuildContext context) async {
+  return await showDialog<AppAction>(
+      context: context,
+      builder: (BuildContext context) {
+        List<Widget> children = [
+          SimpleDialogOption(
+              onPressed: () => Navigator.pop(context, AppAction.addToGrid),
+              child: const Text('Add To Grid')),
+          SimpleDialogOption(
+              onPressed: () =>
+                  Navigator.pop(context, AppAction.openAppSettings),
+              child: const Text('Open App Setting')),
+        ];
+        return SimpleDialog(children: children);
+      });
+}
+
+class AppListState extends State<AppListWidget> {
+  Widget projectWidget() {
+    return Consumer<AppListCacher>(
+      builder: (context, appList, child) {
+        // messageFocus.requestFocus();
+        if (appList.data == null) {
+          // This is very very short lived
+          return const Text(
+            'loading ...',
+            style: TextStyle(fontSize: 32),
+          );
+        }
+
+        List<App> apps = appList.data!.apps.toList();
+        if (widget.filterKey == null) {
+          return const Row();
+        }
+
+        String filterKey = widget.filterKey!;
+
+        bool isFiltered = filterKey != '';
+        apps.sort(
+          (a, b) => a.appName.toLowerCase().compareTo(b.appName.toLowerCase()),
+        );
+
+        apps.removeWhere((app) =>
+            !app.appName.toLowerCase().contains(filterKey.toLowerCase()));
+        print('App length: ${apps.length}');
+        return ListView.builder(
+          reverse: isFiltered,
+          itemCount: apps.length,
+          itemBuilder: (context, index) {
+            App app = apps[index];
+            return GestureDetector(
+              child: ListTile(
+                title:
+                    Text(app.appName, style: const TextStyle(fontSize: 18.0)),
+              ),
+              onTap: () {
+                DeviceApps.openApp(app.packageName);
+                widget.onAppOpen();
+              },
+              onLongPress: () async {
+                AppAction? action = await askAppActions(context);
+                switch (action) {
+                  case AppAction.openAppSettings:
+                    AndroidIntent intent = AndroidIntent(
+                      action: 'action_application_details_settings',
+                      data: 'package:${app.packageName}',
+                      flags: <int>[Flag.FLAG_ACTIVITY_NEW_TASK],
+                    );
+                    intent.launch();
+                  case AppAction.addToGrid:
+                    appList.data!.grid.add(GridApp(app, null));
+                    await appList.flushChanges();
+                  case null:
+                }
+              },
+            );
+          },
+        );
+      },
+    );
+    // messageFocus.requestFocus();
+    // return FutureBuilder(
+    //   initialData: const <App>[],
+    //   builder: (context, projectSnap) {
+    //     // messageFocus.requestFocus();
+    //     if (projectSnap.connectionState == ConnectionState.none &&
+    //         projectSnap.hasData == false) {
+    //       return const Text('connection none');
+    //     }
+
+    //     if (projectSnap.data == null) {
+    //       return const Text('data null');
+    //     }
+
+    //     List<App> apps = projectSnap.data as List<App>;
+
+    //     bool isFiltered = widget.filterKey != '';
+    //     apps.sort(
+    //       (a, b) => a.appName.toLowerCase().compareTo(b.appName.toLowerCase()),
+    //     );
+    //     if (isFiltered) {
+    //       apps = apps.reversed.toList();
+    //     }
+    //     apps.removeWhere((app) => !app.appName
+    //         .toLowerCase()
+    //         .contains(widget.filterKey.toLowerCase()));
+    //     print('App length: ${apps.length}');
+    //     return ListView.builder(
+    //       reverse: isFiltered,
+    //       itemCount: apps.length,
+    //       itemBuilder: (context, index) {
+    //         App app = apps[index];
+    //         return GestureDetector(
+    //           child: ListTile(
+    //             title:
+    //                 Text(app.appName, style: const TextStyle(fontSize: 18.0)),
+    //           ),
+    //           onTap: () => {DeviceApps.openApp(app.packageName)},
+    //           onLongPress: () {
+    //             AndroidIntent intent = AndroidIntent(
+    //               action: 'action_application_details_settings',
+    //               data: 'package:${app.packageName}',
+    //               flags: <int>[Flag.FLAG_ACTIVITY_NEW_TASK],
+    //             );
+    //             intent.launch();
+    //           },
+    //         );
+    //       },
+    //     );
+    //   },
+    //   future: appList.getAppList(),
+    // );
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      body: projectWidget(),
     );
   }
 }
